8bc9ca9d6cfc743571016b364421a213d843ef19
ryo takahashi <rt4869062@gmail.com>
2015-12-13 10:59:22 +0900

    show.html.erbとindex.html.erbの編集

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,7 @@ class Picture < ActiveRecord::Base
   has_many :reviews
 
   has_attached_file :image,
-        styles: { medium: "400x500#", thumb: "100x100#" }
+        styles: { medium: "100x100#", thumb: "100x100#" }
 
   validates_attachment_content_type :image,
         content_type: ["image/jpeg", "image/png"]
