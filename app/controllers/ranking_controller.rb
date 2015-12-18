56fa9f05f74d8f93adcc4e9ea69e71fdfbb94554
ryo takahashi <rt4869062@gmail.com>
2015-12-18 14:16:59 +0900

    review機能の編集とレイアウト調整

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,2 +1,7 @@
 class RankingController < ApplicationController
+
+  def ranking
+    picture_ids = Review.group(:picture_id).order('count_picture_id DESC').count(:picture_id).keys
+    @ranking = picture_ids.map{ |id| Picture.find(id) }
+  end
 end
