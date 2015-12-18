db59bf9e33c79b28a158f70a9a5bec8f28256c84
ryo takahashi <rt4869062@gmail.com>
2015-12-18 19:00:45 +0900

    ランキング機能とviewの追加

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,7 +1,10 @@
 class RankingController < ApplicationController
 
-  def ranking
+  def index
     picture_ids = Review.group(:picture_id).order('count_picture_id DESC').count(:picture_id).keys
     @ranking = picture_ids.map{ |id| Picture.find(id) }
+
+    @pictures = Picture.page(params[:page]).per(100).order("created_at DESC")
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'pictures#index'
-  get '/pictures/men' => 'pictures#men'
-  get '/pictures/women' => 'pictures#women'
 
   resources :users, only: [:show, :edit, :update]
+  resources :ranking, only: :index
+
   resources :pictures, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
