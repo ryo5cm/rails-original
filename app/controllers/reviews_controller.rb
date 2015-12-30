b5eb8755724fa2f6072a5a35dbb03ae63b87cebd
ryo takahashi <rt4869062@gmail.com>
2015-12-30 12:14:19 +0900

    review_controllerのcreateアクションでバリデーション条件分岐

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,19 +2,26 @@ class ReviewsController < ApplicationController
 
   # ログインしていない時は投稿できないようにする
   before_action :authenticate_user!, only:[:new, :create]
+  before_action :set_picture, only:[:new, :create]
 
   def new
-    @picture = Picture.find(params[:picture_id])
     @review = Review.new
   end
 
   def create
-    Review.create(create_params)
-    redirect_to controller: :pictures, action: :index
+    if @picture.create(create_params)
+      redirect_to picture_path(@picture)
+    else
+      render :new
+    end
   end
 
   private
   def create_params
     params.require(:review).permit(:review).merge(picture_id: params[:picture_id], user_id: current_user.id)
+  end
+
+  def set_picture
+    @picture = Picture.find(params[:picture_id])
   end
 end
