a69c068bcf84f43f989afda1fc9c8f19270bcf56
ryo takahashi <rt4869062@gmail.com>
2015-12-31 17:14:11 +0900

    likeモデル、テーブル作成。カラム:user_id, picture_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219124147) do
+ActiveRecord::Schema.define(version: 20151231081152) do
 
   create_table "genres", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "picture_id", limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
   create_table "pictures", force: :cascade do |t|
@@ -27,10 +34,10 @@ ActiveRecord::Schema.define(version: 20151219124147) do
     t.string   "image_content_type", limit: 255
     t.integer  "image_file_size",    limit: 4
     t.datetime "image_updated_at"
-    t.text     "image",              limit: 65535
     t.integer  "user_id",            limit: 4
     t.integer  "gender",             limit: 4
     t.integer  "genre_id",           limit: 4
+    t.text     "image",              limit: 65535
   end
 
   create_table "reviews", force: :cascade do |t|
