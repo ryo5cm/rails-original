4a2830fb8bfeed70781d7cf36d3548d3740348b9
ryo takahashi <rt4869062@gmail.com>
2016-01-01 12:25:23 +0900

    likes_controllerでuser_idとpicture_idを保存。viewで条件分岐による、いいね！表示

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,9 @@
 class LikesController < ApplicationController
 
   def like
-
+    picture = Picture.find(params[:picture_id])
+    like = current_user.likes.build(picture_id: picture.id)
+    like.save
   end
 
   def unlike
